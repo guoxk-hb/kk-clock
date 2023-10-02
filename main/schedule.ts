cb548ed0d14dc67fd7dfbfd74d1056a0563fc7f7
郭现珂 <gxk1744413631@163.com>
2023-10-02 10:55:24 +0800

    完善定时任务

--- main/schedule.ts
+++ main/schedule.ts
@@ -1,12 +1,32 @@
 import schedule from 'node-schedule'
 
 import { readTask } from './nodeApi'
-
+interface task {
+  text: string
+  week: Array<number>
+  date: string
+  switch:boolean
+  edit: boolean
+}
+let jobs: schedule = []
 export async function taskSchedule() {
   const taskList = await readTask()
-  taskList.forEach(item => {
-    schedule.scheduleJob('30 * * * * *', () => {
-      console.log('scheduleCronstyle:' + new Date());
+  console.log('开启定时任务');
+  taskList.forEach((item: task) => {
+    let dateArr = item.date.split(':')
+    let week = item.week
+    let hour = dateArr[0]
+    let minute = dateArr[1]
+    let job: schedule = schedule.scheduleJob({
+      hour: hour,
+      minute: minute,
+      dayOfweek: week,
+    }, () => {
+      console.log('scheduleCronstyle:' + item.text);
+      if(week.length===0){
+      job.cancel()
+      }
     });
+    // jobs.push(job)
   })
 }
